c0aead7b461925dd27c7fe77c51f85c00684007e
Alexandr_Wosmerik <shurik1777@gmail.com>
2023-02-14 22:24:44 +0300

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿int temp = -40;
+
+if (temp <= -10&& temp<=20)
+{
+    Console.WriteLine("Нужна теплая куртка ");
+}
+else if (temp <= -20)
+{
+    Console.WriteLine("Нужна теплая куртка +");
+}
+else if (temp <= -30)
+{
+    Console.WriteLine("Нужна теплая куртка ++");
+}
+// && - оператор который может добавить 2й поддиапозон
